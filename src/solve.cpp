e07b98026c33ad96ebd44759c7f49edeecc399d6
Libor Novak <libor.new@gmail.com>
2016-10-20 17:39:31 +0200

    Initial commit

---
+++ src/solve.cpp
@@ -0,0 +1,24 @@
+//
+// Libor Novak
+// 10/20/2016
+//
+// GPU class, HW1
+// Multi-threaded solver of a system of linear equations.
+//
+//  1. Randomly generate a set of equations Ax=b
+//  2. Gauss eliminate the matrices of the extended system (using a pool of workers)
+//  3. Determine matrix rank
+//  4. Write out the solution
+//
+
+#include <iostream>
+#include <fstream>
+
+
+int main (int argc, char* argv[])
+{
+    std::cout << "hello" << std::endl;
+
+
+    return EXIT_SUCCESS;
+}
