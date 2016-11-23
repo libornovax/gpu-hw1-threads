0a77d9775ce9752b53152f60014cdc608ca42b2a
Libor Novak <libor.new@gmail.com>
2016-11-23 22:24:25 +0100

    Check of filter

--- hw2-filter/src/gpu/GPUFilter.cu
+++ hw2-filter/src/gpu/GPUFilter.cu
@@ -41,8 +41,8 @@ namespace {
         // We can process double the number of cells than threads - each thread reads 2 cells
         __shared__ int cache[2*THREADS_PER_BLOCK];
 
-        cache[2*tid]   = filter(data_array_in[2*tid].key);
-        cache[2*tid+1] = filter(data_array_in[2*tid+1].key);
+        cache[2*tid]   = data_array_in[2*tid].key;
+        cache[2*tid+1] = data_array_in[2*tid+1].key;
 
 
         data_prescan_out[2*tid]   = cache[2*tid];
